4d26cac8084cd850f2345ecb33c0ff7c5ea39a08
unknown <givenng6@gmail.com>
2022-04-21 21:32:24 +0200

    Update

--- Main.js
+++ Main.js
@@ -1,9 +1,9 @@
 let Messenger = require('./Messenger');
 
-let list = ["givenng6", "givenng6.social"];
+let list = [];
 
 for(let i = 0; i < list.length; i++){
-    let send = new Messenger(list[i] + "@gmail.com");
+    let send = new Messenger(list[i]);
 }
 
 
